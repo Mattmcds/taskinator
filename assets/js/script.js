ffc351e57659b85ea57d7ecd5a75119e743e791c
Mattmcds <82618353+Mattmcds@users.noreply.github.com>
2021-05-19 13:59:23 -0600

    form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// COME BACK TO THIS AND FIGURE OUT WHAT IT DOES //
-var createTaskHandler = function(){
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+    console.log(event);
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
